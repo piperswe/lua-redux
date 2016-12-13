2f332df587103aa3a4b73734588d52e8e2ea45a8
Zebulon McCorkle <zebmccorkle@gmail.com>
2016-12-12 18:18:04 -0600

    Fix me not following my own rules

--- example.lua
+++ example.lua
@@ -33,14 +33,14 @@ local store = createStore(counter)
 -- Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly. (no you won't this is Lua)
 -- However it can also be handy to persist the current state in the localStorage. (wat this is Lua we don't have fancy things like that)
 store:subscribe(function()
-  print(store.getState())
+  print(store:getState())
 end)
 
 -- The only way to mutate the internal state is to dispatch an action.
 -- The actions can be serialized, logged or stored and later replayed.
-store.dispatch({ type = "INCREMENT" })
+store:dispatch({ type = "INCREMENT" })
 -- 1
-store.dispatch({ type = "INCREMENT" })
+store:dispatch({ type = "INCREMENT" })
 -- 2
-store.dispatch({ type = "DECREMENT" })
+store:dispatch({ type = "DECREMENT" })
 -- 1
